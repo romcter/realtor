b0bbf464d39632a91408979fdbb3897b3d116ca7
RomanPryimuk <roman.pryimuk@resmed.com>
2022-11-06 01:49:44 +0200

    change controller

--- realor-service/src/main/java/com/startup/realtor/rest/RealtorController.java
+++ realor-service/src/main/java/com/startup/realtor/rest/RealtorController.java
@@ -2,13 +2,14 @@ package com.startup.realtor.rest;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RealtorController {
 
-    @GetMapping("/test")
-    public boolean get(){
-        return true;
+    @GetMapping("/test/{text}")
+    public String get(@PathVariable String test){
+        return test;
     }
 }
